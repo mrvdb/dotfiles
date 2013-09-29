6534d5586e3b3b8f5b5c6a78f67b0b71c83fb23e
Marcel van der Boom <marcel@hsdev.com>
2013-09-29 15:20:35 +0200

    Add pump.io capture to be floating/centered/omnipresent

--- rules.lua
+++ rules.lua
@@ -1,9 +1,9 @@
 --
 -- Awesome client rules
 --
--- Notes: 
+-- Notes:
 -- - matching is case sensitive, use xprop to find out the
---   details on a window 
+--   details on a window
 -- - if the resulting matchlist contain the same
 --   windows and set the same properties the last one gets applied
 
@@ -16,19 +16,20 @@ end
 awful.rules.rules = {
    -- All clients will match this rule, so these are the global settings.
    { rule = { },
-     properties = { 
+     properties = {
 	size_hints_honor = false, -- This removes gaps between windows
 	border_width = beautiful.border_width,
 	border_color = beautiful.border_normal,
 	focus = true,
 	keys = clientkeys,
 	buttons = clientbuttons
-     }  
+     }
    },
 
-   -- Application to Tag placement 
+   -- Application to Tag placement
    { rule = { class = "Claws-mail" },       callback = function(c) c:tags({tags[1][5]}) end},
    { rule = { class = "Google-chrome" },    callback = function(c) c:tags({tags[1][1],tags[1][2]}) end},
+   { rule = { class = "Chromium-browser" }, callback = function(c) c:tags({tags[1][1],tags[1][2]}) end},
    -- Seems newer chrome put this in their class
    { rule = { class = "X-www-browser" },    callback = function(c) c:tags({tags[1][1],tags[1][2]}) end},
    { rule = { class = "Emacs" },            callback = function(c) c:tags({tags[1][1],tags[1][3]}) end},
@@ -36,25 +37,25 @@ awful.rules.rules = {
 
 
    -- Floating applications
-   { rule_any = {class = { "MPlayer", "Gimp", "Psi-plus", "psi", "Remmina"} }, 
-        properties = { floating = true } 
+   { rule_any = {class = { "MPlayer", "Gimp", "Psi-plus", "psi", "Remmina"} },
+        properties = { floating = true }
    },
 
    -- Centered floating applications which stay on top
    { rule_any = {class = {
 		    "Bitcoin", "Totem", "pinentry", "Krb5-auth-dialog", "Gmpc",
 		    "Gtk-recordmydesktop", "Wpa_gui", "Gnuplot",
-		    "Arandr", "Linphone", "Pavucontrol", "coriander", "Coriander"}}, 
-        properties = { floating = true, ontop = true }, 
-   	callback   = awful.placement.centered 
+		    "Arandr", "Linphone", "Pavucontrol", "coriander", "Coriander"}},
+        properties = { floating = true, ontop = true },
+   	callback   = awful.placement.centered
    },
 
    -- Mail compose window is identified by its role property, make it
    -- centralized and floating, but not on top or it will hide the
    -- pinentry window
-   { rule = { role = "compose" }, 
-     properties = { floating = true}, 
-     callback = awful.placement.centered 
+   { rule = { role = "compose" },
+     properties = { floating = true},
+     callback = awful.placement.centered
    },
 
    -- Any window that has the pop-up role should be a pop-up, meaning
@@ -65,7 +66,7 @@ awful.rules.rules = {
    },
 
    -- Claws-mail in its own workspace, but compose on any screen
-   { rule = { role = "compose" }, 
+   { rule = { role = "compose" },
      callback = place_on_all_tags
    },
 
@@ -75,12 +76,12 @@ awful.rules.rules = {
    -- * mail composition
    -- all have the 'Emacs' class, but different instances
    -- The matching uses the Lua string.match() function
-   { rule_any = { instance   = { "capture", "dent", "mailcompose", "unsent mail" }  }, 
-     properties  = { floating = true, ontop = true}, 
-     callback = awful.placement.centered 
+   { rule_any = { instance   = { "capture", "dent", "pump", "mailcompose", "unsent mail" }  },
+     properties  = { floating = true, ontop = true},
+     callback = awful.placement.centered
    },
-   -- ... and appear on every tag 
-   { rule_any = { instance   = { "capture", "dent", "mailcompose", "unsent mail" } }, 
+   -- ... and appear on every tag
+   { rule_any = { instance   = { "capture", "dent", "pump", "mailcompose", "unsent mail" } },
      callback = place_on_all_tags
    }
 }
