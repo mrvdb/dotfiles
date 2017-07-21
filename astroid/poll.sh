2beb54a7e85f13f787f24629adb044a5be75768f
Marcel van der Boom <marcel@hsdev.com>
2017-07-21 13:44:57 +0200

    [astroid] Implement poll.sh
    - mbsync does only a quick Pull/New/Flags on INBOX (should be very fast)
    - run notmuch new, but without hooks
    - but tag (if any) what has been incoming

--- astroid/poll.sh
+++ astroid/poll.sh
@@ -2,3 +2,16 @@
 
 # Astroid poll script
 # runs at interval under astroids control
+# or manually
+HOOKDIR=$HOME/Maildir/.notmuch/hooks
+
+# What I want:
+# - do a very quick sync
+# - run notmuch new, but not the hooks (because that would run mbsync)
+
+# Just pull INBOX and flags
+mbsync hsd-quick
+
+notmuch new --no-hooks
+notmuch tag --batch --input=$HOOKDIR/tag/incoming
+notifymuch
